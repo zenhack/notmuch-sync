d6eee2bc9e19963bbd7ae550ef90c1ff00a9d912
Ian Denhardt <ian@zenhack.net>
2017-03-04 15:40:54 -0500

    Move implementation to a module, clean up a bit.
    some overly-terse/obscure expressions have been expanded to clearer
    code, and we're now using capability discipline/dependency injection.

---
+++ notmuch_sync.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+import sys
+
+
+class Dump(defaultdict):
+
+    def __init__(self):
+        defaultdict.__init__(self, lambda: set())
+
+    @classmethod
+    def from_filename(filename, _open=open):
+        with _open(filename) as f:
+            return Dump.read_from(f)
+
+    @classmethod
+    def read_from(f):
+        text = f.read()
+        lines = text.split('\n')
+        lines = [line.split() for line in lines]
+        dump_dict = Dump()
+        for line in lines:
+            if line == []:  # this might happen at the end of the file.
+                continue
+            ident = line.pop()
+            line.pop()  # get rid of the '--' delimiter
+            dump_dict[ident] = set(line)
+        return dump_dict
+
+    def write_to(self, f):
+        for key in self.keys():
+            f.write(' '.join(list(self[key]) + ['--', key, '\n']))
+        f.close()
+
+
+def merge(ancestor, left, right):
+    result = Dump()
+
+    keys = set()
+    for dump in (ancestor, left, right):
+        for key in dump.keys():
+            keys.add(key)
+
+    for key in keys:
+        added = left[key].union(right[key]).difference(ancestor[key])
+        in_both = left[key].intersection(right[key])
+        result[key] = added.union(in_both)
+    return result
+
+
+def main(argv=sys.argv, exit=exit, stderr=sys.stderr, open=open):
+    if len(argv) != 5:
+        stderr.write(
+            "usage: '%s <ancestor> <left> <right> <result>\n" % argv[0]
+        )
+        exit(1)
+    ancestor, left, right, out = [Dump.from_filename(fname, _open=open)
+                                  for fname in argv[1:4]]
+    result = merge(ancestor, left, right)
+    with open(out) as f:
+        result.write_to(f)
+
+
+if __name__ == '__main__':
+    main()
