0583fe609e9a2117496dff037140588f3080c40e
Ian Denhardt <ian@zenhack.net>
2017-03-04 22:16:57 -0500

    Fix console_scripts path
    This should have been updated when we moved the module.

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='notmuch-sync',
       test_suite='tests',
       entry_points={
           'console_scripts': [
-              'notmuch-sync=notmuch_sync:main',
+              'notmuch-sync=notmuch_sync.dump:main',
           ],
       })
